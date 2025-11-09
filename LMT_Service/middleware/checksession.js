44f71ee444e5b84f8ad701ea9b2e2e5594660f45
kwochengho <kwochengho1020318@gmail.com>
2025-11-10 02:55:54 +0800

    sessionsettings

--- LMT_Service/middleware/checksession.js
+++ LMT_Service/middleware/checksession.js
@@ -4,7 +4,7 @@ function checkSession(req, res, next) {
   }
 
   // 檢查 session
-  if (req.session && req.session.User_ID) {
+  if (req.session) {
     next(); // 已登入，放行
   } else {
     res.status(401).send({ message: 'UnAuthorized' });
