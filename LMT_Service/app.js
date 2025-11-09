d0d6eddc6cece00b8445777e136bb517738b218a
kwochengho <kwochengho1020318@gmail.com>
2025-11-10 03:18:14 +0800

    samesitenone

--- LMT_Service/app.js
+++ LMT_Service/app.js
@@ -35,12 +35,12 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {
+  
     maxAge: 1000 * 60 * 60, // 1 小時
     httpOnly: true,         // 前端無法讀 cookie
     secure: true    ,
     sameSite: "none",
-  } 
+  
 }))
 app.use(checkSession);
 app.use('/', indexRouter);
